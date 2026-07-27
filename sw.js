f888788dc63be04217e986b98afe67e12f72eba2
jlowe022 <jlowe022@users.noreply.github.com>
2026-07-27 15:55:45 +0000

    Add new letter: /ˌrēəˈrānj/ and bump PWA cache to v3

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
+const CACHE_NAME = 'reservoir-v3';
+
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('reservoir-v1').then(cache =>
+    caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
         '/Raymond-s-Reservoir/',
         '/Raymond-s-Reservoir/index.html'
@@ -9,8 +12,22 @@ self.addEventListener('install', e => {
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
